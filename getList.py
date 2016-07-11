945c21da2c1a544994be8cd0f9d49d78b4a81f10
all4win <gongtiancheng2012@gmail.com>
2016-07-11 10:00:41 +0800

    3M

--- getList.py
+++ getList.py
@@ -8,21 +8,24 @@ readme.write('Covered Questions\n')
 readme.write('\n')
 
 out.write('Medium:\n')
-readme.write('* Medium:\n')
+readme.write('* Medium: ')
 medium = os.listdir('C:\\study\\wordpress\\LeetCode\\Medium')
+readme.write(str(len(medium)) + '\n')
 for name in medium:
-    out.write(name[0: -4] + '\n')
-    readme.write('	* ' + name[0: -4] + '\n')
+	out.write(name[0: -4] + '\n')
+	readme.write('	* ' + name[0: -4] + '\n')
+
 
 out.write('\n')
-out.write('Hard:\n')
-
 readme.write('\n')
-readme.write('* Hard:\n')
+
+out.write('Hard:\n')
+readme.write('* Hard: ')
 hard = os.listdir('C:\\study\\wordpress\\LeetCode\\Hard')
+readme.write(str(len(hard)) + '\n')
 for name in hard:
-    out.write(name[0: -4] + '\n')
-    readme.write('	* ' + name[0: -4] + '\n')
+	out.write(name[0: -4] + '\n')
+	readme.write('	* ' + name[0: -4] + '\n')
 
 out.close()
 readme.close()
